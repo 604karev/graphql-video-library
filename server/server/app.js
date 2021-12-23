5a11bec0c5ab625375def0d8484e0ae9526d8234
604karev <604karev@gmail.com>
2021-12-23 14:50:05 +0300

    feat: moved data to mongoDB setting up connection to DB

--- server/server/app.js
+++ server/server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect('mongodb+srv://test_user:test123@graphql.70erq.mongodb.net/GraphQL?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.use('/graphql', graphqlHTTP({
     schema,
