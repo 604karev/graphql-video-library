20f46327870b5697d2a3398df327895098d56583
604karev <604karev@gmail.com>
2021-12-30 14:25:58 +0300

    fix: connect applications with heroku app, created new build

---
+++ application/build/precache-manifest.849ea76b8a344e09d199eabd9ba9789d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ffa876e97bb82ba84c7",
+    "url": "/static/css/main.6d005ce0.chunk.css"
+  },
+  {
+    "revision": "2ffa876e97bb82ba84c7",
+    "url": "/static/js/main.2ffa876e.chunk.js"
+  },
+  {
+    "revision": "727e58640eb535a61722",
+    "url": "/static/js/1.727e5864.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a7534bed981a65d368951a88afcdfbe",
+    "url": "/index.html"
+  }
+];
