24db3107a1ce93b24f85c7104628ee821f89fdf6
Kirill Hrudzinski <604karev@gmail.com>
2023-12-08 10:03:42 +0100

    add new build

--- application/src/App.js
+++ application/src/App.js
@@ -6,7 +6,7 @@ import ApolloClient from 'apollo-boost'
 import {ApolloProvider} from 'react-apollo'
 
 const client = new ApolloClient({
-    uri: `https://frozen-stream-24570.herokuapp.com/graphql`
+    uri: `https://graphql-video-library.onrender.com/graphql`
 });
 
 
