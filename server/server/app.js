622bd6731575379e4e5461756998c19287a2d156
Kirill Hrudzinski <604karev@gmail.com>
2023-12-21 11:05:20 +0100

    depency conflict fix

--- server/server/app.js
+++ server/server/app.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('../schema/schema');
-const mongoose = require('mongoose');
-const conrs = require('cors');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("../schema/schema");
+const mongoose = require("mongoose");
+const conrs = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3005;
 
-mongoose.connect('mongodb+srv://test_user:test123@graphql.70erq.mongodb.net/GraphQL?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const dbConnection = mongoose.connection;
-dbConnection.on('error', err => console.log(`Connection error: ${err}`));
-dbConnection.once('open', () => console.log('Connected to DB!'));
+dbConnection.on("error", (err) => console.log(`Connection error: ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
 app.use(conrs());
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: true
-}));
-
-
-
-app.listen(PORT, err => {
-        err ? console.log(err) : console.log('Server started')
-    }
+    graphiql: true,
+  })
 );
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log("Server started");
+});
